94a984d9632c75804344e5c6754bc7c4c5687414
fatehahmod <ahmod.fateh@gmail.com>
2022-03-22 23:23:57 +0600

    added new

---
+++ index.js
@@ -0,0 +1,63 @@
+const express=require('express');
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
+const app=express();
+const ObjectId=require('mongodb').ObjectId;
+const port=process.env.PORT || 5000 
+const cors =require('cors')
+// medilware
+app.use(cors());
+app.use(express.json());
+
+
+
+// cannect uri
+const uri = `mongodb+srv://jm-hospital:7ypATjeqI71vxb6T@cluster0.n0kiz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+async function run (){
+    try {
+        await client.connect();
+        const database = client.db('m-hospital');
+        const finddoctorCallection = database.collection('finddoctor');
+        const servicedoctorCallection = database.collection('service');
+       
+        
+
+        // get api
+
+        app.get('/service',async(req,res)=>{
+            const cursor=servicedoctorCallection.find({});
+            const user=await cursor.toArray();
+          
+            res.send(user)
+            
+        });
+
+
+        app.get('/finddoctor',async(req,res)=>{
+            const cursor=finddoctorCallection.find({});
+            const user=await cursor.toArray();
+          
+            res.send(user)
+            
+        });
+
+     }
+
+    finally{
+         // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+
+app.get('/',(req,res)=>{
+    res.send('runing server 5000')
+});
+app.listen(port,()=>{
+    console.log('live server',port);
+})
